942abaef294f635e8f16120a306f29bc00e2ce6e
Oryam <48219965+or-yam@users.noreply.github.com>
2020-12-08 16:01:23 +0200

    day3 part2

--- day3/day3.js
+++ day3/day3.js
@@ -15,4 +15,35 @@ const countTrees = (input) => {
   return treeCounter;
 };
 
-console.log(countTrees(field));
+// console.log(countTrees(field));
+
+const countTrees2 = (input, right, down) => {
+  const rowLength = input[0].length - 1;
+  let treeCounter = 0;
+  let currentRow = 0;
+
+  for (let i = 0; i < input.length; i += down) {
+    input[i][currentRow] === '#' && treeCounter++;
+    currentRow = (currentRow + right) % rowLength;
+  }
+
+  return treeCounter;
+};
+
+const slopes = [
+  { right: 1, down: 1 },
+  { right: 3, down: 1 },
+  { right: 5, down: 1 },
+  { right: 7, down: 1 },
+  { right: 1, down: 2 },
+];
+
+const allTreesEncounter = () => {
+  let res = 1;
+  for (const slope of slopes) {
+    res *= countTrees2(field, slope.right, slope.down);
+  }
+  return res;
+};
+
+console.log(allTreesEncounter());
